f5b2f680da79ce7146989c8064703e2aa4d3091e
Philipp Hofer <philipp.hofer@aol.com>
2021-12-30 16:39:41 +0100

    initial

---
+++ src/main/java/de/philipphofer/website/controller/PageController.java
@@ -0,0 +1,27 @@
+package de.philipphofer.website.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @GetMapping("/share")
+    public String share(@RequestParam String id, Model model) {
+        model.addAttribute("id", id);
+        return "share";
+    }
+}
---
+++ startup.sh
@@ -0,0 +1,2 @@
+mvn clean package
+java -jar target/sudoku-server-0.0.1-SNAPSHOT.jar &
