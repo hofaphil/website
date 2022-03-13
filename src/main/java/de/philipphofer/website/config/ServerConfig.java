db5cd30099d78c75f56a0d36c30d699d41630a62
Philipp Hofer <philipp.hofer@aol.com>
2022-03-13 19:26:41 +0100

    new settings

--- src/main/java/de/philipphofer/website/config/ServerConfig.java
+++ src/main/java/de/philipphofer/website/config/ServerConfig.java
@@ -1,11 +1,9 @@
 package de.philipphofer.website.config;
 
-import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.apache.coyote.ajp.AjpNioProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,29 +12,20 @@ import org.springframework.context.annotation.Profile;
 @Profile("prod")
 public class ServerConfig {
 
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(getHttpConnector());
-        return tomcat;
-    }
+    @Value("${AJP_SECRET}")
+    private String secret;
 
-    private Connector getHttpConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(80);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
+    @Bean
+    public TomcatServletWebServerFactory servletContainer() {
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector ajpConnector = new Connector("org.apache.coyote.ajp.AjpNioProtocol");
+        AjpNioProtocol protocol = (AjpNioProtocol) ajpConnector.getProtocolHandler();
+        protocol.setSecret(secret);
+        ajpConnector.setPort(4000);
+        ajpConnector.setSecure(true);
+        tomcat.addAdditionalTomcatConnectors(ajpConnector);
+
+        return tomcat;
     }
 }
