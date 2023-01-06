77473b059a58a14278a53325e0ce84ae2f98cd19
Philipp Hofer <philipp.hofer@aol.com>
2023-01-06 22:48:31 +0100

    fixed wrong button sizing

--- src/components/dropdown/dropdownButton.tsx
+++ src/components/dropdown/dropdownButton.tsx
@@ -8,16 +8,14 @@ export interface Props {
 
 export default function DropdownButton(props: Props) {
   return (
-    <span>
-      <button className={"btn btn-outline-light btn-lg m-1 dropdown-toggle"} data-bs-toggle="dropdown" aria-expanded="false">
-        {!!props.icon &&
-          <i className={"me-2 bi " + props.icon} aria-hidden="true"></i>
-        }
-        {props.text}
-      </button>
+    <button className={"btn btn-outline-light btn-lg m-1 dropdown-toggle"} data-bs-toggle="dropdown" aria-expanded="false">
+      {!!props.icon &&
+        <i className={"me-2 bi " + props.icon} aria-hidden="true"></i>
+      }
+      {props.text}
       <ul className="dropdown-menu">
         {props.children}
       </ul>
-    </span>
+    </button>
   )
 }
