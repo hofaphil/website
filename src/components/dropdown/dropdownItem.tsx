0c7498122410734c8a0ab0d2057ae7e7108e9f79
Philipp Hofer <philipp.hofer@aol.com>
2023-02-11 11:51:25 +0100

    changed corner radius of dropdown

--- src/components/dropdown/dropdownItem.tsx
+++ src/components/dropdown/dropdownItem.tsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 export interface Props {
   text: string
   link: string
@@ -10,7 +8,7 @@ export interface Props {
 export default function DropdownItem(props: Props) {
   return (
     <li className="m-2">
-      <a className="dropdown-item" href={props.link} target={props.blank ? "_blank" : "_self"} rel="noreferrer" style={{borderRadius: "5px"}}>
+      <a className="dropdown-item" href={props.link} target={props.blank ? "_blank" : "_self"} rel="noreferrer" style={{borderRadius: "0.375rem"}}>
         {!!props.icon &&
           <i className={"me-2 bi " + props.icon} aria-hidden="true"></i>
         }
